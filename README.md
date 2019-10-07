5b9e5f685abe23b3105e8e7fa3d35a508f422590
Rodrigo Oliveira <rodrigo.oliveira@byne.com.br>
2019-10-07 17:19:13 -0300

    PA-2589 - Add Jack and Plug examples

--- README.md
+++ README.md
@@ -4,7 +4,20 @@ ZMQ based microservice communications library
 Two transports are supported: **ipc** (though *IPCEndpoint*, using the following path
 as default: */tmp/jack.plug*), and **tcp** (*TCPEndpoint*, using *3559* as the default
 port number).
-```
+
 ## Requirements
-    * pyzmq
+    * pyzmq 15.4.0
     * simb.pilsner
+
+## Examples
+An example of the use of this library can be found on the *examples* folder, and
+running them is pretty straightforward:
+
+Run *examples/jack.py* and *examples/plug.py*, in different terminals. You
+should set the same transport argument on both of them (**ipc** or **tcp**).
+
+```
+    $ python examples/jack.py ipc
+    --
+    $ python examples/plug.py ipc
+```
---
+++ examples/plug.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Copyright (c) 2016 BYNE. All rights reserved.
+
+This example subclass PlugBase to act as a plug ("server").
+"""
+
+import sys
+from jackplug.plug import PlugBase
+from jackplug.utils import IPCEndpoint
+from jackplug.utils import TCPEndpoint
+
+
+class PlugTest(PlugBase):
+    def __init__(self, *args, **kwargs):
+        super(PlugTest, self).__init__(*args, **kwargs)
+
+        self.on_timeout(self._timeout_occurred)
+        self.start()
+
+    def _timeout_occurred(self, service):
+        print("Timeout occurred on service (plug) %s!" % service)
+
+    def recv(self, service, message):
+
+        print("Recv (%s): %s" % (message, service))
+        print("Ok, answering :)")
+
+        self.send(service, message)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Thou shalt run as: %s [ipc | tcp]" % sys.argv[0])
+        sys.exit()
+
+    use_ipc = False
+    use_tcp = False
+
+    if sys.argv[1] == 'ipc':
+        print("Using IPC transport")
+        use_ipc = True
+    elif sys.argv[1] == 'tcp':
+        print("Using TCP transport")
+        use_tcp = True
+    else:
+        print("Unknown argument: %s" % sys.argv[1])
+        sys.exit()
+
+    print("Acting as Plug")
+
+    endpoint = None
+    if use_ipc:
+        endpoint = IPCEndpoint(pathname="/tmp/jack.plug.test")
+    elif use_tcp:
+        endpoint = TCPEndpoint(address="*", port="1234")
+
+    plug = PlugTest(endpoint=endpoint)
+    print("Exiting Plug...")
