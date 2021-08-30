5046b37eab9a5f592cf1242ae585de0509227191
Rodrigo Oliveira <rodrigo.oliveira@byne.com.br>
2021-08-30 15:11:41 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="JackPlug",
-    version="0.1",
+    version="0.2",
     author="Rodrigo Oliveira",
     author_email="rodrigo@byne.com.br",
     packages=find_packages(),
